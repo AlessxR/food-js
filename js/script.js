a38b8a3b84c8ea3e546e21137eab2a68d476157c
Alexxoooo <alexandaliev33@gmail.com>
2025-07-04 12:40:42 +0300

    Tabs

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// Получаем элементы
+const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+
+
+// Скрытие контента
+function hideTabContent() {
+
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    });
+};
+
+function showTabContent(index = 0) {
+    tabsContent[index].classList.add('show', 'fade');
+    tabsContent[index].classList.remove('hide');
+    tabs[index].classList.add('tabheader__item_active');
+}
+
+
+hideTabContent();
+showTabContent();
+
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    // Клик по элементу с классом tabheader__item
+    if (target && target.classList.contains('tabheader__item')) {
+        
+        tabs.forEach((item, index) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(index);
+            }
+        });
+    }
+}); 
