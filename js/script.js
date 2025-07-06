b0ca4f43a091ff35c0a4ea8a342fc1189a503db3
Alexxoooo <alexandaliev33@gmail.com>
2025-07-06 15:51:31 +0300

    Modal

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
 
+const modalOpenBtn = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]'),
+    modalTimerId = setTimeout(toggleModal, 5000);
+
 const deadline = '2025-07-12';
 
 
@@ -37,7 +42,7 @@ tabsParent.addEventListener('click', (event) => {
 
     // Клик по элементу с классом tabheader__item
     if (target && target.classList.contains('tabheader__item')) {
-        
+
         tabs.forEach((item, index) => {
             if (target == item) {
                 hideTabContent();
@@ -49,11 +54,17 @@ tabsParent.addEventListener('click', (event) => {
 
 // Timer
 function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-        minutes = Math.floor((t / 1000 / 60) % 60),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+        days = hours = minutes = seconds = 0;
+    } else {
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+        hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        minutes = Math.floor((t / 1000 / 60) % 60);
         seconds = Math.floor((t / 1000) % 60);
+    }
 
     return {
         t,
@@ -97,3 +108,32 @@ function setClock(selector, endtime) {
 };
 
 setClock('.timer', deadline);
+
+// Modal Window
+function toggleModal() {
+    // modal.classList.toggle('show');
+    // modal.classList.toggle('hide');
+
+    if (modalOpenBtn) {
+        modal.classList.toggle('show');
+    } else {
+        modal.classList.toggle('hide')
+    }
+
+    clearInterval(modalTimerId);
+
+}
+
+modalOpenBtn.forEach(item => {
+    item.addEventListener('click', toggleModal);
+});
+modalCloseBtn.addEventListener('click', toggleModal);
+
+function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+        toggleModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+}
+
+window.addEventListener('scroll', showModalByScroll);
