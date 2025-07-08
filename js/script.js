6930877da5a8868789e398cb8b8356373eb62502
Alexxoooo <alexandaliev33@gmail.com>
2025-07-08 13:01:46 +0300

    Fetch API Use

--- js/script.js
+++ js/script.js
@@ -220,32 +220,56 @@ function postData(form) {
         // form.append(statusMessage);
         form.insertAdjacentElement('afterend', statusMessage);
 
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json');
+        // OLD
+        // const request = new XMLHttpRequest();
+        // request.open('POST', 'server.php');
+        // request.setRequestHeader('Content-type', 'application/json');
 
         const formData = new FormData(form); // Собираем данные с определенной формы
         console.log(formData);
 
         const object = {};
-
         formData.forEach(function (value, key) {
             object[key] = value;
         });
-        const json = JSON.stringify(object);
 
-        request.send(json); // Отправляем форму на сервер теперь
+        // const json = JSON.stringify(object);
 
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                showThanksModal(formsMessage.success);
-                form.reset();
-                statusMessage.remove();
-            } else {
-                showThanksModal(formsMessage.failure);
-            }
+        // request.send(json); // Отправляем форму на сервер теперь
+
+        // NEW FETCH
+        fetch('server.php', {
+            method: 'POST',
+            // headers: {
+            //     'Content-type': 'application/json'
+            // },
+            body : JSON.stringify(object)
+        })
+        .then(data => data.text())
+        .then(data => {
+            console.log(data);
+            showThanksModal(formsMessage.success);
+            // form.reset();
+            statusMessage.remove();    
+        })
+        .catch(() => {
+            showThanksModal(formsMessage.failure);
+        })
+        .finally(() => {
+            form.reset();
         });
+
+
+        // request.addEventListener('load', () => {
+        //     if (request.status === 200) {
+        //         console.log(request.response);
+        //         showThanksModal(formsMessage.success);
+        //         form.reset();
+        //         statusMessage.remove();
+        //     } else {
+        //         showThanksModal(formsMessage.failure);
+        //     }
+        // });
     });
 }
 
